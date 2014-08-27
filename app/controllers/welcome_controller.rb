59d3f0f1b84aa4dc498e70b604e9160e8605c14c
Charles <charles.swedensky@gmail.com>
2014-08-26 23:08:49 -0400

    tweaking forem

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,7 +5,26 @@ class WelcomeController < ApplicationController
   end
   def new_user
   end
-  def forums
+  def forums_politics
+    redirect_to "/forums/categories/general"
+  end
+  def forums_nature
+    redirect_to "/forums/categories/nature"
+  end
+  def forums_business
+    redirect_to "/forums/categories/business"
+  end
+  def forums_tv
+    redirect_to "/forums/categories/tv"
+  end
+  def forums_sports
+    redirect_to "/forums/categories/sports"
+  end
+  def forums_science
+    redirect_to "/forums/categories/science"
+  end
+  def forums_games
+    redirect_to "/forums/categories/games"
   end
   def keyone
     @prophecy = Prophecy.new(params[:prophecy])
